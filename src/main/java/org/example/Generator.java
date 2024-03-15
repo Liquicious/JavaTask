4a145dacd1b830c0356124258e9c9197bf29f8bc
Liquicious <korolkevich03@yandex.ru>
2024-03-15 10:15:28 +0400

    v1.0

--- src/main/java/org/example/Generator.java
+++ src/main/java/org/example/Generator.java
@@ -1,8 +1,130 @@
 package org.example;
 
+import java.lang.reflect.Field;
+import java.lang.management.ManagementFactory;
+
 public class Generator {
-    public static <T> T generate(T inputClass)
+    private static long m = (long)Math.pow(2, 31);
+    private static long a = 1103515245;
+    private static long c = 12345;
+    private long seed;
+    public Generator()
     {
-        return inputClass;
+        this.seed = ManagementFactory.getRuntimeMXBean().getStartTime();
+    }
+    private void updateSeed()
+    {
+        seed = (a * seed + c) % m;
+    }
+    public <T> void generate(T inputClass)
+    {
+        Field[] ClassFields = inputClass.getClass().getDeclaredFields();
+        for (Field classField : ClassFields)
+        {
+            classField.setAccessible(true);
+            // boolean, byte, short, int , long, double, float, char, string
+            if (classField.getType().equals(boolean.class))
+            {
+                try
+                {
+                    classField.set(inputClass, (seed % 2 == 1));
+                    updateSeed();
+                }
+                catch (IllegalAccessException e)
+                {
+                    throw new RuntimeException();
+                }
+            }
+            else if (classField.getType().equals(byte.class))
+            {
+                try
+                {
+                    classField.set(inputClass, (byte) (seed % Byte.MAX_VALUE));
+                    updateSeed();
+                }
+                catch (IllegalAccessException e)
+                {
+                    throw new RuntimeException();
+                }
+            }
+            else if (classField.getType().equals(int.class))
+            {
+                try
+                {
+                    classField.set(inputClass, (int) (seed % Integer.MAX_VALUE));
+                    updateSeed();
+                }
+                catch (IllegalAccessException e)
+                {
+                    throw new RuntimeException();
+                }
+            }
+            else if (classField.getType().equals(long.class))
+            {
+                try
+                {
+                    classField.set(inputClass, (int) (seed % Long.MAX_VALUE));
+                    updateSeed();
+                }
+                catch (IllegalAccessException e)
+                {
+                    throw new RuntimeException();
+                }
+            }
+            else if (classField.getType().equals(double.class))
+            {
+                try
+                {
+                    classField.set(inputClass, seed * 0.0000000001);
+                    updateSeed();
+                }
+                catch (IllegalAccessException e)
+                {
+                    throw new RuntimeException();
+                }
+            }
+            else if (classField.getType().equals(float.class))
+            {
+                try
+                {
+                    classField.set(inputClass, (float) (seed * 0.0000000001));
+                    updateSeed();
+                }
+                catch (IllegalAccessException e)
+                {
+                    throw new RuntimeException();
+                }
+            }
+            else if (classField.getType().equals(char.class))
+            {
+                try
+                {
+                    classField.set(inputClass, (char) (seed % Character.MAX_VALUE));
+                    updateSeed();
+                }
+                catch (IllegalAccessException e)
+                {
+                    throw new RuntimeException();
+                }
+            }
+            else if (classField.getType().equals(String.class))
+            {
+                try
+                {
+                    StringBuilder tmp = new StringBuilder();
+                    for (int i = 0; i < seed % 100; i++)
+                    {
+                        updateSeed();
+                        tmp.append((char) (seed % Character.MAX_VALUE));
+                    }
+                    updateSeed();
+                    classField.set(inputClass, tmp.toString());
+                }
+                catch (IllegalAccessException e)
+                {
+                    throw new RuntimeException();
+                }
+            }
+        }
     }
 }
---
+++ src/main/java/org/example/TestClass.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class TestClass {
+    // boolean, byte, short, int , long, double, float, char, string
+    private boolean bo;
+    private byte by;
+    private short sh;
+    private int in;
+    private long lo;
+    private double dou;
+    private float fl;
+    private char ch;
+    private String st;
+    TestClass()
+    {
+        bo = true;
+        by = (byte) 12;
+        sh = 13;
+        in = 14;
+        lo = 15;
+        dou = 1.6;
+        fl = (float) 1.7;
+        ch = 'a';
+        st = "string";
+    }
+    public void show()
+    {
+        System.out.println(bo);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(in);
+        System.out.println(lo);
+        System.out.println(dou);
+        System.out.println(fl);
+        System.out.println(ch);
+        System.out.println(st);
+    }
+}
